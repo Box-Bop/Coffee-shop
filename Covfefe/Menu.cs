eae87ede97561d07cb6dd13b6f910f584ea24796
SchoolPC <Box-Bop@github.com>
2018-02-01 09:27:51 +0200

    start

---
+++ Covfefe/Menu.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Covfefe
+{
+    class Menu
+    {
+        public double Cost;
+        public string Ingridients;
+        public string Type;
+        public string Name;
+
+        public void HotChocolate()
+        {
+            Cost = 2;
+            Ingridients = "Hot milk\nCocoa power";
+            Type = "Hot drink";
+            Name = "Hot chocolate";
+        }
+
+        public void Coffee()
+        {
+            Cost = 1.3;
+            Ingridients = "Coffee powder\nHot water";
+            Type = "Hot drink";
+            Name = "Generic coffee";
+        }
+
+        public void Ekspresso()
+        {
+            Cost = 1;
+            Ingridients = "1 coffee bean\nLava";
+            Type = "Hot drink";
+            Name = "Ekspresso";
+        }
+
+        public void Buy()
+        {
+            Console.WriteLine("Ok! Some " + Name + ". Anything else?\n");
+        }
+    }
+}
---
+++ Covfefe/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Covfefe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome!\nWhat would you like to order?\n");
+            bool start = true;
+            while (start == true)
+            {
+                var menu = new Menu();
+                Console.WriteLine("We have:\nHot chocolate (1)\nCoffee (2)\nEkspresso (3)");
+                int dec = Convert.ToInt32(Console.ReadLine());
+
+                if (dec == 1)
+                {
+                    menu.HotChocolate();
+                    menu.Buy();
+                }
+                if (dec == 2)
+                {
+                    menu.Coffee();
+                    menu.Buy();
+                }
+                if (dec == 3)
+                {
+                    menu.Ekspresso();
+                    menu.Buy();
+                }
+
+
+            }
+
+        }
+    }
+}
